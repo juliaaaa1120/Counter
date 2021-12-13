d373dbef5828b27130013e93f823b7ea79e685c2
Julia Ng <ngju@oocl.com>
2021-12-13 15:01:46 +0800

    [Counter][JuliaNg] - feat: finalize the counter

--- myscripts.js
+++ myscripts.js
@@ -1,17 +1,18 @@
-var number1 = 0;
-var number2 = 0;
 function onFirstButtonClicked(action) {
+    var number1 = parseInt(document.getElementById("number1").innerHTML);
     action == "plus" ? ++number1 : --number1;
-    document.getElementById("number1").innerHTML = number1;
+    document.getElementById("number1").innerHTML = number1.toString();
     sum();
 }
 
 function onSecondButtonClicked(action) {
+    var number2 = parseInt(document.getElementById("number2").innerHTML);
     action == "plus" ? ++number2 : --number2;
-    document.getElementById("number2").innerHTML = number2;
+    document.getElementById("number2").innerHTML = number2.toString();
     sum();
 }
 
 function sum() {
-    document.getElementById("sum").innerHTML = "sum: " + (number1 + number2);
+    var sum = parseInt(document.getElementById("number1").innerHTML) + parseInt(document.getElementById("number2").innerHTML);
+    document.getElementById("sum").innerHTML = "sum: " + sum.toString();
 }
